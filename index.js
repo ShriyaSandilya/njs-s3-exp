f541fe7dc04112b80c15a58152b87c9f6203a575
ShriyaSandilya <83954402+ShriyaSandilya@users.noreply.github.com>
2021-05-29 22:33:09 +0530

    Upload code

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/number', (req, res) => {
+    res.send('Go to /number/:num to check if :num is odd or even');
+})
+
+app.get('/number/:num' , (req, res) => {
+    param = req.params;
+    if (param['num'] %2 == 0){
+        res.send(`The number ${param['num']} is even`)
+    }
+    else{
+        res.send(`The number ${param['num']} is odd`)
+    }
+})
+
+app.listen(5000, () => console.log('Running'))
